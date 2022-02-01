6a3ad092e15eaf43e18d2ec049446efcab371ba7
Robinson Rojas <totorox123@gmail.com>
2022-01-31 22:05:05 -0500

    add function

---
+++ src/function.js
@@ -0,0 +1,21 @@
+var x = 1;
+var y = 2;
+
+function suma(a, b) {
+  s = a + b;
+  return s;
+}
+
+console.log("La suma es : " + suma(x, y));
+
+//-----------------------------
+
+var x = 1;
+var y = 2;
+
+function resta(a, b) {
+  s = a - b;
+  console.log("La resta es: " + s);
+}
+
+resta(x, y);
